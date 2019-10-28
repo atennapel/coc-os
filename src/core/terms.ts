dbec1725d116f1a2472a55e8602f7635978081be
Albert ten Napel <aptennap@gmail.com>
2019-10-28 21:53:19 +0100

    vals

--- src/core/terms.ts
+++ src/core/terms.ts
@@ -4,12 +4,14 @@ export type Term
   | { tag: 'App', left: Term, impl: boolean, right: Term }
   | { tag: 'Abs', type: Term, impl: boolean, body: Term }
   | { tag: 'Pi', type: Term, impl: boolean, body: Term }
+  | { tag: 'Let', type: Term, impl: boolean, val: Term, body: Term }
   | { tag: 'Type' };
 
 export const Var = (index: Ix): Term => ({ tag: 'Var', index });
 export const App = (left: Term, impl: boolean, right: Term): Term => ({ tag: 'App', left, impl, right });
 export const Abs = (type: Term, impl: boolean, body: Term): Term => ({ tag: 'Abs', type, impl, body });
 export const Pi = (type: Term, impl: boolean, body: Term): Term => ({ tag: 'Pi', type, impl, body });
+export const Let = (type: Term, impl: boolean, val: Term, body: Term): Term => ({ tag: 'Let', type, impl, val, body });
 export const Type: Term = { tag: 'Type' };
 
 export const flattenApp = (t: Term): [Term, [boolean, Term][]] => {
@@ -57,5 +59,7 @@ export const showTerm = (t: Term): string => {
     const [as, b] = flattenPi(t);
     return `/${as.map(([im, t]) => im ? `{${showTerm(t)}}` : showTermP(t.tag === 'Abs' || t.tag === 'Pi' || t.tag === 'App', t)).join(' ')}. ${showTerm(b)}`;
   }
+  if (t.tag === 'Let')
+    return `let ${showTerm(t.type)} = ${showTerm(t.val)} in ${showTerm(t.body)}`;
   return t;
 };
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 import { Pi, showTerm, Type, Var } from './core/terms';
+import { normalize } from './core/vals';
+
+/**
+ * TODO:
+ * - let expressions, showTerm fix
+ */
 
 const term = Pi(Type, true, Pi(Var(0), false, Var(1)));
 console.log(showTerm(term));
+const norm = normalize(term);
+console.log(showTerm(norm));
