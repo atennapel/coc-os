a374574b34bd0707739b9320f25f75fc092a1cbd
Albert ten Napel <aptennap@gmail.com>
2019-10-31 09:27:50 +0100

    testing

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,12 @@ import { typecheck } from './core/typecheck';
 
 const v = Var;
 
+const Nat = Pi(Type, true, Pi(v(0), false, Pi(Pi(v(1), false, v(2)), false, v(2))));
+const z = Abs(Type, true, Abs(v(0), false, Abs(Pi(v(1), false, v(2)), false, v(1))));
+const s = Abs(Nat, false, Abs(Type, true, Abs(v(0), false, Abs(Pi(v(1), false, v(2)), false, App(v(0), false, App(App(App(v(3), true, v(2)), false, v(1)), false, v(0)))))));
+
 try {
-  const term = App(Abs(Type, true, Abs(v(0), false, v(0))), true, Type);
+  const term = App(s, false, App(s, false, App(s, false, z)));
   console.log(showTerm(term));
   const ty = typecheck(term);
   console.log(showTerm(ty));
@@ -21,6 +25,8 @@ try {
   console.log(showTerm(norm));
   const erased = erase(term);
   console.log(showETerm(erased));
+  const normerased = erase(norm);
+  console.log(showETerm(normerased));
 } catch (err) {
   console.log('' + err);
 }
