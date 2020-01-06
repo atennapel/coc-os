f6b5c95b7a003242a522d04c290fc46d1262d3c7
Albert ten Napel <aptennap@gmail.com>
2020-01-06 20:59:39 +0100

    fix wip

--- bundle.js
+++ bundle.js
@@ -363,10 +363,12 @@ const check = (ts, vs, tm, ty_) => {
         const term = check(list_1.Cons([x, exports.BoundT(ty.type)], ts), vals_1.extendV(vs, x, maybe_1.Nothing), tm, ty.body(vx));
         return syntax_1.Abs(x, true, vals_1.quote(ty.type, vs), term);
     }
+    /*
     if (ty.tag === 'VFix' && tm.tag !== 'Roll') {
-        const term = check(ts, vs, tm, ty.body(ty));
-        return syntax_1.Roll(vals_1.quote(ty, vs), term);
+      const term = check(ts, vs, tm, ty.body(ty));
+      return Roll(quote(ty, vs), term);
     }
+    */
     if (tm.tag === 'Hole')
         return newMeta(ts);
     if (tm.tag === 'Open') {
