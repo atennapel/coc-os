23bd7cdce46a6a026df75a63b1ceb65140a99567
Albert ten Napel <aptennap@gmail.com>
2020-01-30 21:17:33 +0100

    math test

--- src/index.ts
+++ src/index.ts
@@ -22,8 +22,14 @@ const nats: Term[] = [z]; for (let i = 0; i <= 10; i++) nats.push(App(s, nats[i]
 const tsnat = Fix(Type, Pi(Type, Pi(Var(0), Pi(Pi(Var(2), Var(2)), Var(2)))));
 // @ts-ignore
 const sz = Roll(tsnat, Abs(Type, Abs(Var(0), Abs(Pi(tsnat, Var(2)), Var(1)))));
+// @ts-ignore
+const add = Abs(tnat, Abs(tnat, App(App(App(Var(1), tnat), Var(0)), s)));
+// @ts-ignore
+const mul = Abs(tnat, Abs(tnat, App(App(App(Var(1), tnat), z), App(add, Var(0)))));
+// @ts-ignore
+const pow = Abs(tnat, Abs(tnat, App(App(App(Var(0), tnat), App(s, z)), App(mul, Var(1)))));
 
-const tm = Abs(tsnat, App(Unroll(Var(0)), Type));
+const tm = App(App(pow, nats[2]), nats[3]);
 console.log(showTerm(tm));
 const ty = typecheck(tm, Nil, Nil, 0);
 console.log(showTerm(ty));
